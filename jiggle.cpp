3161d275fd686cfa8857437952a3b9c61d229552
Oliver Lau <oliver@ersatzworld.net>
2022-12-12 16:23:59 +0100

    Reformatted code.

--- jiggle.cpp
+++ jiggle.cpp
@@ -1,51 +1,61 @@
 #include <ApplicationServices/ApplicationServices.h>
 #include <IOKit/IOKitLib.h>
 
-int64_t systemIdleSecs(void) {
-    int64_t idlesecs = -1;
-    io_iterator_t iter = 0;
-    if (IOServiceGetMatchingServices(kIOMainPortDefault, IOServiceMatching("IOHIDSystem"), &iter) == KERN_SUCCESS) {
-        io_registry_entry_t entry = IOIteratorNext(iter);
-        if (entry) {
-            CFMutableDictionaryRef dict = NULL;
-            if (IORegistryEntryCreateCFProperties(entry, &dict, kCFAllocatorDefault, 0) == KERN_SUCCESS) {
-                CFNumberRef obj = (CFNumberRef) CFDictionaryGetValue(dict, CFSTR("HIDIdleTime"));
-                if (obj) {
-                    int64_t nanoseconds = 0;
-                    if (CFNumberGetValue(obj, kCFNumberSInt64Type, &nanoseconds)) {
-                        idlesecs = nanoseconds / 1000000000LL;
-                    }
-                }
-                CFRelease(dict);
-            }
-            IOObjectRelease(entry);
+int64_t systemIdleSecs(void)
+{
+  int64_t idlesecs = -1;
+  io_iterator_t iter = 0;
+  if (IOServiceGetMatchingServices(kIOMainPortDefault, IOServiceMatching("IOHIDSystem"), &iter) == KERN_SUCCESS)
+  {
+    io_registry_entry_t entry = IOIteratorNext(iter);
+    if (entry)
+    {
+      CFMutableDictionaryRef dict = NULL;
+      if (IORegistryEntryCreateCFProperties(entry, &dict, kCFAllocatorDefault, 0) == KERN_SUCCESS)
+      {
+        CFNumberRef obj = (CFNumberRef)CFDictionaryGetValue(dict, CFSTR("HIDIdleTime"));
+        if (obj)
+        {
+          int64_t nanoseconds = 0;
+          if (CFNumberGetValue(obj, kCFNumberSInt64Type, &nanoseconds))
+          {
+            idlesecs = nanoseconds / 1000000000LL;
+          }
         }
-        IOObjectRelease(iter);
+        CFRelease(dict);
+      }
+      IOObjectRelease(entry);
     }
-    return idlesecs;
-}    
+    IOObjectRelease(iter);
+  }
+  return idlesecs;
+}
 
-CGPoint getMousePos() {
+CGPoint getMousePos()
+{
   CGEventRef event = CGEventCreate(NULL);
   CGPoint cursor = CGEventGetLocation(event);
   CFRelease(event);
   return cursor;
 }
 
-void moveMouseTo(const CGPoint point) {
+void moveMouseTo(const CGPoint point)
+{
   CGEventRef event = CGEventCreateMouseEvent(
-    NULL, kCGEventMouseMoved, point, kCGMouseButtonLeft);
+      NULL, kCGEventMouseMoved, point, kCGMouseButtonLeft);
   CGEventPost(kCGHIDEventTap, event);
   CFRelease(event);
 }
 
-
-int main() {
+int main()
+{
   const int64_t IDLE_TIMEOUT_SECS = 60;
-  while (true) {
+  while (true)
+  {
     int64_t idleSecs = systemIdleSecs();
     CGPoint currentPos = getMousePos();
-    if (idleSecs >= IDLE_TIMEOUT_SECS) {
+    if (idleSecs >= IDLE_TIMEOUT_SECS)
+    {
       moveMouseTo(CGPointMake(currentPos.x + 1, currentPos.y));
       moveMouseTo(currentPos);
     }
