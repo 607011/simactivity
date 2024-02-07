ae63a47ea781b73642c8b15ba1e9887b2c2c2d4a
Oliver Lau <oliver@ersatzworld.net>
2024-02-07 13:56:58 +0100

    Windows version ready. Needs testing.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
+#endif
+
+#ifdef _MSC_VER
+#include <Windows.h>
+#endif
 
 #include "fake.h"
 
@@ -14,6 +21,7 @@ int main(int argc, char *argv[])
 #else
     const unsigned int CHECK_INTERVAL_SECS = 10;
 #endif
+    unsigned int check_interval_secs = CHECK_INTERVAL_SECS;
     int idle_timeout_secs =
         (argc == 2)
             ? atoi(argv[1])
@@ -28,7 +36,11 @@ int main(int argc, char *argv[])
         {
             fake_user_event();
         }
-        sleep(CHECK_INTERVAL_SECS);
+#ifdef _MSC_VER
+        Sleep(1000U * check_interval_secs);
+#else
+        sleep(check_interval_secs);
+#endif    
     }
     return EXIT_SUCCESS;
 }
